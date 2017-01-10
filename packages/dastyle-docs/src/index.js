6558d02c07680624042a60002410f1595d5339c3
Elad Amsalem <elad.ams@gmail.com>
2017-01-10 11:15:25 +0200

    add gh-pages deployment scripts

--- packages/dastyle-docs/src/index.js
+++ packages/dastyle-docs/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route, browserHistory } from 'react-router';
+import { Router, Route, hashHistory } from 'react-router';
 import { render } from 'react-dom';
 import DocPage from './components/doc-page';
 import items from './content';
@@ -9,12 +9,12 @@ import './index.css';
 
 const _ = require('lodash');
 
-
+console.log('loaded');
 render(
-  <Router history={browserHistory}>
+  <Router history={hashHistory}>
     <Route path="/" component={Layout} items={items} labels={labels}>
       {_.keys(items).map(docName =>
-        <Route key={docName} path={docName} component={DocPage} contentEntry={items[docName]} />,
+        <Route key={docName} path={`/${docName}`} component={DocPage} contentEntry={items[docName]} />,
       )}
     </Route>
   </Router>
